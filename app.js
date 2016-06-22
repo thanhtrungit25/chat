22f4979e79498a4a3a27bd1d040d54437c15d64c
Thanh Trung <thanhtrung.it25@gmail.com>
2016-06-22 22:18:27 +0700

    simple chat with socket.io

---
+++ app.js
@@ -0,0 +1,35 @@
+/**
+ * Module dependencies.
+ */
+
+var express = require('express')
+  , routes = require('./routes') // load routes module in routes folder index.js
+  , http = require('http')
+  , path = require('path');
+
+var app = express();
+
+app.configure(function() {
+  app.set('port', process.env.PORT || 3000);
+  app.set('views', __dirname + '/views');
+  app.set('view engine', 'jade');
+  app.use(express.favicon());
+  app.use(express.logger('dev'));
+  app.use(express.bodyParser());
+  app.use(express.methodOverride());
+  app.use(express.static(path.join(__dirname, 'public')));
+  app.use(app.router);
+});
+
+app.configure('development', function(){
+  app.use(express.errorHandler());
+});
+
+app.get('/', routes.index);
+
+var server = http.createServer(app);
+server.listen(app.get('port'), function () {
+  console.log("Express server listening on port " + app.get('port'));
+});
+
+require('./routes/sockets.js').initialize(server);
