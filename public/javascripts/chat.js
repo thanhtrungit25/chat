23b3d9e3cd06f91c73d2a4396c12a80990875b79
Thanh Trung <thanhtrung.it25@gmail.com>
2016-06-23 10:56:11 +0700

    Chat with username broastcast to other user

--- public/javascripts/chat.js
+++ public/javascripts/chat.js
@@ -1,20 +1,1 @@
-var socket = io.connect('/');
-
-socket.on('message', function (data) {
-  data = JSON.parse(data);
-  $('#messages').append('<div class="'+ data.type +'">'+ data.message +'</div>');
-
-});
-
-$(function() {
-  $('#send').click(function () {
-    var data = {
-      message: $('#message').val(),
-      type: 'userMessage'
-    };
-
-    socket.send(JSON.stringify(data));
-    $('#message').val('');
-  });
-});
-
+var socket = io.connect('/');socket.on('name_set', function (data) {  $('#nameform').hide();  $('#messages').append('<div class="systemMessage">'+ 'Hello ' + data.name +'</div>');  $('#send').click(function () {    var data = {      message: $('#message').val(),      type: 'userMessage'    };    socket.send(JSON.stringify(data));    $('#message').val('');  });  socket.on('message', function (data) {    data = JSON.parse(data);    if (data.username) {      $('#messages').append('<div class="'+ data.type +'"><span class="name">'+ data.username +':</span>'+ data.message +'</div>');    }    else {      $('#messages').append('<div class="'+ data.type +'">' + data.message +'</div>');    }  });});$(function() {  $('#setname').click(function () {    socket.emit("set_name", {name: $('#nickname').val()});  });});
--- routes/sockets.js
+++ routes/sockets.js
@@ -4,17 +4,34 @@ exports.initialize = function(server) {
   io = io.listen(server);
 
   io.sockets.on("connection", function(socket){
-    // serser send message
-    socket.send(JSON.stringify({type:'serverMessage', message: 'Welcome to the most interesting chat room on earth!'}));
 
     socket.on('message', function(message){
+
       message= JSON.parse(message);
+      
       if(message.type == "userMessage"){
-        socket.broadcast.send(JSON.stringify(message));
-        message.type = "myMessage";
-        socket.send(JSON.stringify(message));
+
+        if (socket.nickname) {
+          message.username = socket.nickname;
+          socket.broadcast.send(JSON.stringify(message));
+          message.type = "myMessage";
+          socket.send(JSON.stringify(message));
+        }
+
       }
+
     });
+
+    socket.on('set_name', function (data) {
+
+      socket.nickname = data.name;
+      socket.emit('name_set', data);
+        socket.send(JSON.stringify({type:'serverMessage',
+          message: 'Welcome to the most interesting chat room on earth!'}));
+
+    })
+
+
   });
 
 }
