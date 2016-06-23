4e5503f0490b1bee2a6820646d110d3f268da7b0
Thanh Trung <thanhtrung.it25@gmail.com>
2016-06-23 21:52:44 +0700

    Add namspaces, organizaion code.

--- routes/sockets.js
+++ routes/sockets.js
@@ -1,38 +1,76 @@
 var io = require('socket.io');
+var sessionMng = require('../modules/sessionManagement');
+var nicknames = [];
 
 exports.initialize = function(server) {
   io = io.listen(server);
 
-  io.sockets.on("connection", function(socket){
+// namspace chat infrastration
+  var chatInfra = io.of("/chat_infra")
+    .on("connection", function(socket){
 
-    socket.on('message', function(message){
+      socket.on('set_name', function (data, callback) {
 
-      message= JSON.parse(message);
-      
-      if(message.type == "userMessage"){
+        if (nicknames.indexOf(data.name) != -1) { //exist
+          callback(false);
+        } else {
 
-        if (socket.nickname) {
-          message.username = socket.nickname;
-          socket.broadcast.send(JSON.stringify(message));
-          message.type = "myMessage";
-          socket.send(JSON.stringify(message));
+          socket.nickname = data.name;
+          nicknames.push(socket.nickname);
+
+          socket.emit('name_set', data); // emit event handler to client
+          updateNicknames();
+          updateBroadcastNicknames();
+
+          socket.send(JSON.stringify({type:'serverMessage',
+              message: 'Welcome to the most interesting chat room on earth!'}));
+          socket.broadcast.emit('user_entered', data);
         }
 
+      }); // end set_name handler
+
+      function updateNicknames() {
+        if (socket.nickname) {
+          socket.emit('usernames', nicknames);
+        }
       }
+      function updateBroadcastNicknames() {
+        if (socket.nickname) {
+          socket.broadcast.emit('usernames', nicknames);
+        }
+      }
+
+      socket.on('disconnect', function (data) {
+        if (!socket.nickname) return false;
+        nicknames.splice(nicknames.indexOf(socket.nickname), 1);
+        updateBroadcastNicknames();
+      });
 
     });
 
-    socket.on('set_name', function (data) {
+// namspace chat communication
+    var chatCom = io.of("/chat_com")
+      .on("connection", function(socket) {
 
-      socket.nickname = data.name;
-      socket.emit('name_set', data);
-        socket.send(JSON.stringify({type:'serverMessage',
-          message: 'Welcome to the most interesting chat room on earth!'}));
-      socket.broadcast.emit('user_entered', data);
+        socket.on('message', function(message){
 
-    })
+          message = JSON.parse(message);
+          if(message.type == "userMessage"){
 
+            if (message.name) {
+              message.username = message.name;
 
-  });
+              // broastcast to other client
+              socket.broadcast.send(JSON.stringify(message));
+
+              message.type = "myMessage";
+              socket.send(JSON.stringify(message));
+            }
+
+          }
+
+        }); // end event handler receive message from client
+
+      })
 
 }
